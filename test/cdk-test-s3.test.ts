9c91f69a3961546cb3835082a680e2c0981fbaf2
pisa <pisa0401mst@gmail.com>
2022-11-28 05:18:23 +0900

    Initial commit

---
+++ test/cdk-test-s3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkTestS3 from '../lib/cdk-test-s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-test-s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkTestS3.CdkTestS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
